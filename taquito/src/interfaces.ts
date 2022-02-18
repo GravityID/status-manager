c40eec41259a27e42f142a7273a34a0eda849d7e
François Guérin <francois.guerin@gravity.earth>
2022-02-18 15:46:10 +0100

    Remove taquito folder

--- taquito/src/interfaces.ts
+++
@@ -1,9 +0,0 @@
-import { MichelsonMapKey } from "@taquito/michelson-encoder";
-import { MichelsonMap } from "@taquito/taquito";
-import BigNumber from "bignumber.js";
-
-export interface RevocationManagerStorage {
-  owner: string;
-  list: BigNumber;
-  metadata: MichelsonMap<MichelsonMapKey, unknown>;
-}
